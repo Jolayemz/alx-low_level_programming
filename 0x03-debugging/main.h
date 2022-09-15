14f92585d67b7919c3429a77e7ff5fed0d52f464
oladejijolayemz@gmail.com <oladejijolayemz@gmail.com>
2022-09-15 16:55:50 -0700

    0_main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,10 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>                                
+#include <stdio.h>
+
 void positive_or_negative(int i);
+void print_remaining_days(int, int,  int); 
+int convert_day(int, int);
 
 #endif /* MAIN_H */
